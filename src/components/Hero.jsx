0e78a419a9420640a2f3447a59e7332bcb854707
ElJefe111944 <jamesfarr1@sky.com>
2022-10-22 12:09:04 +0000

    Hero component add & styled

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AiOutlineSearch } from 'react-icons/ai'
 
 import beachVid from '../assets/beachVid.mp4'
 
@@ -8,11 +9,20 @@ const Hero = () => {
         {/* Video */}
         <video className='w-full object-cover h-full' muted loop autoPlay src={beachVid}></video>
           {/* Overlay */}
-        <div className='absolute w-full h-full top-0 left-0 bg-gray-900/20'></div>
+        <div className='absolute w-full h-full top-0 left-0 bg-gray-900/15'></div>
           {/* Search Bar Title */}
           <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
             <h1>First Class Travel</h1>
-            <h2>Top 1% locations Worldwide</h2>
+            <h2 className='py-4'>Top 1% locations Worldwide</h2>
+            {/* Search Bar */}
+            <form className='flex justify-between items-center max-w-[700px] mx-auto w-full border p-1 rounded-md text-black bg-gray-100/90'>
+              <div>
+              <input type="text" placeholder='Search Destinations' className='bg-transparent w-[300px] sm:w-[400px] focus:outline-none' />
+              </div>
+              <div>
+                <button><AiOutlineSearch style={{color: 'white'}} size={20} className='icon' /></button>
+              </div>
+            </form>
           </div>
     </div>
   )
